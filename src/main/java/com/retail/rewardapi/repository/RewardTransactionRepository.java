2792809c3badbe2657cb533194c061dcab6b1837
RahulDere <dere.rahu@gmail.com>
2023-07-05 09:06:59 -0400

    "Added code with H2 repository"

--- src/main/java/com/retail/rewardapi/repository/RewardTransactionRepository.java
+++ src/main/java/com/retail/rewardapi/repository/RewardTransactionRepository.java
@@ -1,2 +1,9 @@
-package com.retail.rewardapi.repository;public class RewardTransactionRepository {
+package com.retail.rewardapi.repository;
+
+import com.retail.rewardapi.model.RewardTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RewardTransactionRepository extends JpaRepository<RewardTransaction, Long> {
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO rewardtransaction(transactionId, customerId, customerName, transactionAmount, createdDate)
+values(1L, 1000L, "Rahul Dere", 100L, LocalDate.of(2023, 6, 28));
+INSERT INTO rewardtransaction(transactionId, customerId, customerName, transactionAmount, createdDate)
+values(2L,1001L, "Rahul Kadu", 100L, LocalDate.of(2023, 6, 27));
+INSERT INTO rewardtransaction(transactionId, customerId, customerName, transactionAmount, createdDate)
+values(3L,1002L, "Ravi Kumar", 60L, LocalDate.of(2023, 6, 27));
+INSERT INTO rewardtransaction(transactionId, customerId, customerName, transactionAmount, createdDate)
+values(4L,1003L, "Mattew Mike", 400L, LocalDate.of(2023, 6, 26));
+INSERT INTO rewardtransaction(transactionId, customerId, customerName, transactionAmount, createdDate)
+values(5L, 1004L, "Shruti Kopad", 155L, LocalDate.of(2023, 6, 28));
+INSERT INTO rewardtransaction(transactionId, customerId, customerName, transactionAmount, createdDate)
+values(6L, 1005L, "Simran Singh", 225L, LocalDate.of(2023, 6, 29));
